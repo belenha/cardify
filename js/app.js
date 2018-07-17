9b2d898dd99608539412c4b10a109dc3e420e864
Zetnavrek <vykovi@gmail.com>
2018-07-17 12:45:00 -0500

    Haciendo merge

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 const pictures = require("index.js");
 
-$(document).ready(function(){
-
+$(document).ready(function() {
+	$('.figure').text('<figure>');
+	$('.img').text('<img>');
+	$('.figcaption').text('<figcaption>');
+  $('.step1').text('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>');
+  $('#step2').text('<link rel="stylesheet" href="path-to-main.css">');
+  $('#step3').text('<link rel="stylesheet" href="path-to-index.js">');
+  $('#step4').text('<div class="imgwrap"><img src="path-to-image.jpg" alt="my image"></div>');
+  $('#step5').text('<img class="imgCardify" src="path-to-image.jpg" alt="my image"></div>');
 });
-
-$(container).cardify({});
